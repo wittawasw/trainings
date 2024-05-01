628663581a6e52b1d36266df76cecb359851bb67
wittawasw <start@wittawasw.com>
2024-05-01 14:15:17 +0700

    create user

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_01_040119) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_01_065739) do
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.text "body"
     t.string "summary", limit: 500
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
